1dfee5b42c364d560022442ba614e58d923b2252
Kort <de.k0rt@yahoo.de>
2022-02-07 00:41:46 +0300

    com

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,22 @@
-$(document).ready(function(){
-  $('.carousel__inner').slick({
-    infinite: true,
-    speed: 300,
-    // adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/left.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="img/slider/right.png"></button>',
-    responsive: [{
-        breakpoint: 992,
-        settings: {
-        dots: true,
-        arrows: false
-      }
-    }]
+
+
+let slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  navPosition: 'bottom',
+  responsive: {
+    768: {
+      dots: true,
+      arrows: false
+    },
   }
-  );
 });
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');})
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');})
