ff4617e1545dbd5aaf29a52f814733c82d0bd237
Kort <de.kort@yahoo.de>
2022-02-07 20:17:25 +0300

    com

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+  
+});
 
 
 let slider = tns({
@@ -15,8 +24,14 @@ let slider = tns({
   }
 });
 
+
 document.querySelector('.prev').addEventListener('click', function () {
   slider.goTo('prev');})
 
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');})
+
+
+
+
+ 
